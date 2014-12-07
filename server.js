7fca0e10f45ef7ca27d5d97438713150d0475062
Florian Rathgeber <florian.rathgeber@gmail.com>
2014-12-07 12:16:17 +0000

    Convert SVG to ps and plot when saving (not working)

--- server.js
+++ server.js
@@ -1,8 +1,9 @@
-var fs = require('fs');
-var path = require('path');
-var express = require('express')
-var sassMiddleware = require('node-sass-middleware')
-var bodyParser = require('body-parser')
+var fs = require('fs'),
+  path = require('path'),
+  exec = require('child_process').exec,
+  express = require('express'),
+  sassMiddleware = require('node-sass-middleware'),
+  bodyParser = require('body-parser')
 
 var app = module.exports = express();
 
@@ -25,7 +26,25 @@ app.get('/', function(req, res) {
 });
 
 app.post('/plot', function(req, res) {
-  console.log(req.body.svg);
+  fs.writeFile('/tmp/plot.svg', req.body.svg, function(err) {
+    if (err) {
+      console.log(err);
+    } else {
+      inkscape = exec('inkscape -P /tmp/plot.ps /tmp/plot.svg', function(err, stdout, stderr) {
+        console.log('inkscape stdout:', stdout);
+        console.log('inkscape stderr:', stderr);
+        if (err)
+          console.log('inkscape failed with', err);
+        else
+          exec('cat /tmp/plot.ps | graphtecprint', function(err, stdout, stderr) {
+            console.log('graphtecprint stdout:', stdout);
+            console.log('graphtecprint stderr:', stderr);
+            if (err)
+              console.log('graphtecprint failed with', err);
+          });
+      });
+    }
+  });
 });
 
 app.listen(process.env.PORT || 4000);
